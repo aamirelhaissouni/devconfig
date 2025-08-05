f03283d54ce1e01e03228429fcf94cc384be8996
Aamir Elhaissouni <elhaissouniaamir@gmail.com>
2025-08-05 10:59:51 -0400

    Update keymaps.lua

--- nvim/lua/hamood/core/keymaps.lua
+++ nvim/lua/hamood/core/keymaps.lua
@@ -22,6 +22,20 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+-- stop delete from yanking at the same time:
+
+-- Prevent 'd' from copying to the register
+keymap.set({ "n", "v" }, "d", '"_d', { noremap = true, silent = true, desc = "Delete without copying" })
+
+-- Prevent 'dd' from copying to the register
+keymap.set("n", "dd", '"_dd', { noremap = true, silent = true, desc = "Delete line without copying" })
+
+-- Prevent 'x' from copying to the register
+keymap.set({ "n", "v" }, "x", '"_x', { noremap = true, silent = true, desc = "Delete character without copying" })
+
+-- Prevent 'dw' from copying to the register
+keymap.set("n", "dw", '"_dw', { noremap = true, silent = true, desc = "Delete word without copying" })
+
 
 
 
